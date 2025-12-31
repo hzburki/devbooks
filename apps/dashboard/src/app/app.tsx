578e10aecea02b664081c090604d4745ce38c455
Haseeb Zia Burki <hzburki.hzb@gmail.com>
2025-12-31 13:09:40 +0500

    added tailwind

--- apps/dashboard/src/app/app.tsx
+++ apps/dashboard/src/app/app.tsx
@@ -1,28 +1,10 @@
-import NxWelcome from './nx-welcome';
-
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="@org/dashboard" />
+    <div className="p-4 text-gray-800">
+      <div className="text-3xl font-bold underline">Hello World</div>
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
       <Routes>
         <Route
           path="/"
